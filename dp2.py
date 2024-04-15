ce159f86553c648dbc4f64bdef8a5ba00ddfe56f
divyajmk kuruvilla <yaq7fm@virginia.edu>
2024-04-15 17:49:36 +0000

    finished script and created count text file

--- dp2.py
+++ dp2.py
@@ -12,22 +12,45 @@ uri = "mongodb+srv://cluster0.pnxzwgz.mongodb.net/"
 client = MongoClient(uri, username='nmagee', password=MONGOPASS, connectTimeoutMS=200, retryWrites=True)
 
 database = "yaq7fm"
-collection = "dataproject2files"
+collection_name = "dataproject2files"
 
 # Switch to UVA computing ID database
 db = client[database]
 
 # Create a new collection
-new_collection = db[collection]
+collection = db[collection_name]
 
-all_files = []
+total_docs = 0
+successful_imports = 0
+failed_imports = 0
+corrupted_files = 0
 # List Directory Contents
 # Iterate through each file in the folder
 for f in os.listdir("data"):
-    file = os.path.join("data",f)
+    file_path = os.path.join("data",f)
     # Now open and read each JSON files
-    with open(file, 'r') as file:
-        files = json.load(file)
-        all_files.append(files)
+    with open(file_path, 'r') as file:
+        try:
+            # Try reading in json files 
+            files = json.load(file)
+            # Importing JSON file into MongoDB
+            # Insert loaded data into collection
+            if isinstance(files, list):
+                collection.insert_many(files)
+                total_docs += len(files)
+            else:
+                collection.insert_one(files)
+                total_docs += 1
+        except json.JSONDecodeError as e:
+            # Error handling for when loading JSON file
+            corrupted_files += 1
+        except Exception as e:
+            if isinstance(files,list):
+                failed_imports += len(files)
+            else:
+                failed_imports += 1
 
-print(all_files)
+print("Total documents imported:", total_docs)
+print("Failed imports:", failed_imports)
+print("Corrupted documents:", corrupted_files)
+
